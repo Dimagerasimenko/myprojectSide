cd3a316f9a120e4c5eb8ab56fda40b9d35f82621
Dimagerasimenko <holy_priest@mail.ru>
2018-12-09 19:40:16 +0300

    first com

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var server = require('browser-sync').create();
+
+gulp.task('css', function(){
+  return gulp.src('style/style.less')
+  .pipe(plumber())
+  .pipe(less())
+  .pipe(postcss([
+    autoprefixer()
+  ]))
+.pipe(gulp.dest('css'))
+.pipe(server.stream());
+});
+
+gulp.task('server', function () {
+  server.init({
+    server: '',
+    notify: false,//уведомления
+    open: true,
+    cors: true,//кроссдоменный запрос
+    ui: false
+});
+
+gulp.watch('style/**/*.less', gulp.series('css'));
+
+gulp.watch('**/*.html').on('change', server.reload);
+
+
+});
+
+gulp.task('start', gulp.series('css', 'server'));
